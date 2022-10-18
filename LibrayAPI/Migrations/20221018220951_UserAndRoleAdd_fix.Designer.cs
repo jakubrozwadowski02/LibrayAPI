2ed8fe71712e71273f17f8cf3567136462f91473
Jakub Rozwadowski <jakub.rozwadowski02@gmail.com>
2022-10-19 00:10:59 +0200

    Small fix - Add User and Role models

--- LibrayAPI/Migrations/20221018220951_UserAndRoleAdd_fix.Designer.cs
+++ LibrayAPI/Migrations/20221018220951_UserAndRoleAdd_fix.Designer.cs
@@ -4,6 +4,7 @@ using LibrayAPI.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace LibrayAPI.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20221018220951_UserAndRoleAdd_fix")]
+    partial class UserAndRoleAdd_fix
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- LibrayAPI/Model/UserModel.cs
+++ LibrayAPI/Model/UserModel.cs
@@ -5,7 +5,7 @@ namespace LibrayAPI.Model
     {
         public int Id { get; set; }
         public string FirstName { get; set; }
-        public int LastName { get; set; }
+        public string LastName { get; set; }
         public string Email { get; set; }
         public string PasswordHash { get; set; }
         public string PhoneNumber { get; set; }
