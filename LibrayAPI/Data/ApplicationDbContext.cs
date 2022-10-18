697bc4a43b3dae22cf6eec9ddc7771dd1f861b6b
Jakub Rozwadowski <jakub.rozwadowski02@gmail.com>
2022-10-19 00:08:20 +0200

    Add User and Role model

--- LibrayAPI/Data/ApplicationDbContext.cs
+++ LibrayAPI/Data/ApplicationDbContext.cs
@@ -12,6 +12,19 @@ namespace LibrayAPI.Data
         public DbSet<CategoryModel> Categories { get; set; }
         public DbSet<AuthorModel> Authors { get; set; }
         public DbSet<BookModel> Books { get; set; }
+        public DbSet<UserModel> Users { get; set; }
+        public DbSet<RoleModel> Roles { get; set; }
 
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<UserModel>()
+                .Property(u => u.Email)
+                .IsRequired();
+
+            modelBuilder.Entity<RoleModel>()
+                .Property(r => r.Name)
+                .IsRequired();
+        }
     }
 }
---
+++ LibrayAPI/Model/UserModel.cs
@@ -0,0 +1,17 @@
+﻿
+namespace LibrayAPI.Model
+{
+    public class UserModel
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public int LastName { get; set; }
+        public string Email { get; set; }
+        public string PasswordHash { get; set; }
+        public string PhoneNumber { get; set; }
+        public DateTime CreationDate { get; set; } = DateTime.Now;
+
+        public int RoleId { get; set; }
+        public RoleModel Role { get; set; }
+    }
+}
